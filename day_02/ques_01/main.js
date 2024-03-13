62aa0690f9c75abd2dfa8a1b501b6bb9520f7aed
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-03-14 03:59:30 +0500

    commit

--- day_02/ques_01/main.js
+++ day_02/ques_01/main.js
@@ -1,1 +1,1 @@
-console.log("\"Albert Einstein once said, \"A person who never made a mistake nrver tried anything new.\"\"");
+console.log('Albert Einstein once said, "A person who never made a mistake nrver tried anything new."');
