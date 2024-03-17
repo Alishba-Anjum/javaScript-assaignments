7fba15f687411f19382ad45d3a41a3d073d8c8a3
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-03-18 01:53:41 +0500

    commit

---
+++ day_09/ques_03/main.js
@@ -0,0 +1,11 @@
+//nested if_else
+var aleinColor = "yellow";
+if (aleinColor == "green") {
+    console.log("green color");
+}
+else if (aleinColor == "pink") {
+    console.log("pink color");
+}
+else if (aleinColor == "yellow") {
+    console.log("yellow color");
+}
---
+++ day_09/ques_03/main.ts
@@ -0,0 +1,12 @@
+//nested if_else
+let aleinColor: string = "yellow";
+
+if (aleinColor == "green") {
+    console.log("green color");
+} 
+else if (aleinColor == "pink") {
+    console.log("pink color");
+}
+ else if (aleinColor == "yellow") {
+    console.log("yellow color");
+}
