f64884b06e35fe0455c41fe9a682fea103a19aea
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-04-18 00:05:15 +0500

    commit

--- day_39/ques_01/main.ts
+++ day_39/ques_01/main.ts
@@ -1,7 +1,29 @@
-function dayOfWeek(dayNUmber: number):void{
-    switch(dayNUmber){
-        case 1:
-            console.log("Sunday");
-            
-    }
+function dayOfWeek(dayNUmber: number): void {
+  switch (dayNUmber) {
+    case 1:
+      console.log("Sunday");
+      break;
+    case 2:
+      console.log("Monday");
+      break;
+    case 3:
+      console.log("Tuesday");
+      break;
+    case 4:
+      console.log("Wednesday");
+      break;
+    case 5:
+      console.log("Thursday");
+      break;
+    case 6:
+      console.log("Friday");
+      break;
+    case 7:
+      console.log("Saturday");
+      break;
+    default:
+      console.log("Invalid day number");
+      break;
+  }
 }
+dayOfWeek(3)
