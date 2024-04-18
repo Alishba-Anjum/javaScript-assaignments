0229cb701521178d07885e05923efd46cc31c77b
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-04-19 03:41:59 +0500

    day_41 added

---
+++ day_41/ques_02/main.ts
@@ -0,0 +1,8 @@
+let count: number = 10;
+while (count >= -1) {
+  if (count === 5) {
+    break;
+  }
+  console.log(count);
+  count--;
+}
---
+++ day_41/ques_03/main.js
@@ -0,0 +1,13 @@
+function logUntilVowel(str) {
+    var vowels = "aeiouAEIOU";
+    for (var _i = 0, str_1 = str; _i < str_1.length; _i++) {
+        var char = str_1[_i];
+        if (vowels.includes(char)) {
+            console.log("First vowel found: ".concat(char));
+            break;
+        }
+        console.log(char);
+    }
+}
+logUntilVowel("syzygy");
+logUntilVowel("ffhga");
