009eadb0c99dc85bd974da06dad4f5f9b5202a29
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-04-19 03:19:03 +0500

    day_40 added

---
+++ day_40/ques_03/main.js
@@ -0,0 +1,5 @@
+var movies = ["Snow White", "Beauty and the beast", "Finding Nemo"];
+for (var _i = 0, movies_1 = movies; _i < movies_1.length; _i++) {
+    var movie = movies_1[_i];
+    console.log(movie);
+}
---
+++ day_40/ques_03/main.ts
@@ -0,0 +1,5 @@
+let movies: string[] = ["Snow White", "Beauty and the beast", "Finding Nemo"];
+for(let movie of movies){
+    console.log(movie);
+    
+}
