fd463f50863cb4de1c90d07b4587955a04f62c19
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-04-20 23:53:54 +0500

    day_42 ques_01

---
+++ day_42/ques_01/main.js
@@ -0,0 +1,7 @@
+var person = {
+    name: "Alishba",
+    getName: function () {
+        return this.name;
+    },
+};
+console.log(person.getName());
---
+++ day_42/ques_01/main.ts
@@ -0,0 +1,8 @@
+const person ={
+    name : "Alishba",
+    getName: function(){
+        return this.name;
+    },
+};
+
+console.log(person.getName());
