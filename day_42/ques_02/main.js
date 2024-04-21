58ead0cc90d2f4b27d738927695e214f01aedfcf
Alishba-Anjum <alishbaahmedkhan11@gmail.com>
2024-04-21 19:56:27 +0500

    day_42 ques_02, 03 added

---
+++ day_42/ques_02/main.js
@@ -0,0 +1,8 @@
+var rectangle = {
+    length: 5,
+    width: 6,
+    calcculateArea: function () {
+        return this.length * this.width;
+    },
+};
+console.log(rectangle.calcculateArea());
---
+++ day_42/ques_02/main.ts
@@ -0,0 +1,9 @@
+const rectangle ={
+    length: 5,
+    width: 6,
+    calcculateArea: function(){
+        return this.length * this.width;
+    },
+};
+
+console.log(rectangle.calcculateArea());
